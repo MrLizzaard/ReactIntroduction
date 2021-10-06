30ab80d2de4d6592b2a48e551ba7e964ac355a37
MrLizzaard <dhk21512@gmail.com>
2021-10-06 22:02:56 +0900

    scroll problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
 import Main from "./components/Main/main";
 
 const App = (props) => {
+  const contain = useRef(null);
+  const [scrollY, setScrollY] = useState(0);
+  const bodyHeight = document.body.scrollHeight;
+  const [YOffset, setYOffset] = useState(0);
+  // contain.current.scrollHeight
+
+  const onScroll = () => {
+    console.log(bodyHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <Header />
         <div className="popInfo mainPage"></div>
-        <div className="scrollContainer" data-scroll="true">
+        <div className="scrollContainer" data-scroll="true" ref={contain}>
           <div className="contentsWrap">
             <Switch>
               <Route exact path="/">
--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Main = (props) => {
   return (
-    <div>
-      <h1>main</h1>
-    </div>
+    <>
+      <div className="main m1">maincontent</div>
+      <div className="main m2">maincontent</div>
+      <div className="main m3">maincontent</div>
+      <div className="main m4">maincontent</div>
+    </>
   );
 };
 
