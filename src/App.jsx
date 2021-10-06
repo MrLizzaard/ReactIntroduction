13ac58d12e5eaf270a9688f08ec18fdfc2d63089
MrLizzaard <dhk21512@gmail.com>
2021-10-06 19:17:07 +0900

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/footer";
 import Header from "./components/Header/header";
+import Main from "./components/Main/main";
 
 const App = (props) => {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="popInfo mainPage"></div>
+        <div className="scrollContainer" data-scroll="true">
+          <div className="contentsWrap">
+            <Switch>
+              <Route exact path="/">
+                <Main />
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </Router>
   );
 };
 
