3ab0f8230fa6985679694b4e6b7073ae5a686680
MrLizzaard <dhk21512@gmail.com>
2021-10-13 10:33:33 +0900

    m1 font

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Main = (props) => {
   return (
     <>
-      <div className="main m1">maincontent</div>
+      <div className="main m1">
+        <div className="topTitle">
+          FR
+          <span className="medium">ONT</span>
+          <span className="light">END</span>
+        </div>
+        <div className="bottomTitle">
+          <span className="light">DEV</span>
+          <span className="medium">ELO</span>
+          PER
+        </div>
+      </div>
       <div className="main m2">maincontent</div>
       <div className="main m3">maincontent</div>
       <div className="main m4">maincontent</div>
